31ec06835f02ad863fcc8e6064683e8f9201299c
Lorenzo Tomas Diez <lorenzotomasdiez@gmail.com>
2022-11-07 20:46:11 -0600

    T3: upload photo

--- sections/home/HomeProduct.tsx
+++ sections/home/HomeProduct.tsx
@@ -1,26 +1,41 @@
-import { styled } from '@mui/material';
+import { Container, Stack, styled, Typography } from '@mui/material';
 import { m } from 'framer-motion';
 import React from 'react'
+import { MotionViewport, varFade } from '../../components';
 
 const RootStyle = styled('div')(({ theme }) => ({
-  position: 'relative',
-  backgroundColor: theme.palette.grey[400],
-  [theme.breakpoints.up('md')]: {
-    top: 0,
-    left: 0,
-    width: '100%',
-    height: '100vh',
-    display: 'flex',
-    position: 'fixed',
-    alignItems: 'center',
-  },
+  padding: theme.spacing(24, 0),
 }));
 
 const HomeProduct = () => {
   return (
-    <RootStyle>
-      
-    </RootStyle>
+    <MotionViewport disableAnimatedMobile={false}>
+      <RootStyle>
+        <Container>
+          <Stack spacing={2} justifyContent="center" alignItems="center">
+            <m.div variants={varFade().inUp}>
+              <Typography
+                component="div"
+                variant="overline"
+                sx={{ mb: 2, color: 'text.disabled' }}
+              >
+                Complete product
+              </Typography>
+            </m.div>
+            <m.div variants={varFade().inUp}>
+              <Typography variant="h2" sx={{ mb: 3 }}>
+                Your entire interactive web <br />
+                With you own admin panel
+              </Typography>
+            </m.div>
+            <m.img              
+              variants={varFade().inRight}
+              src={`https://minimal-assets-api-dev.vercel.app/assets/images/home/ic_sketch.svg`}
+            />
+          </Stack>
+        </Container>
+      </RootStyle>
+    </MotionViewport>
   )
 }
 
